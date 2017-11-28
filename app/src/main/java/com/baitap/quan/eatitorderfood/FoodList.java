20d5ac5ceadd43c453e2f60b95508e998aaea9b1
dquand91@gmail.com <dquand91@gmail.com>
2017-11-28 23:00:18 +0700

    - Update Search Food function

--- app/src/main/java/com/baitap/quan/eatitorderfood/FoodList.java
+++ app/src/main/java/com/baitap/quan/eatitorderfood/FoodList.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,9 +14,16 @@ import com.baitap.quan.eatitorderfood.Listener.ItemClickListenerCustom;
 import com.baitap.quan.eatitorderfood.Model.Food;
 import com.baitap.quan.eatitorderfood.ViewHolder.FoodViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.baitap.quan.eatitorderfood.Common.KEY_INTENT_FOOD_ID;
 import static com.baitap.quan.eatitorderfood.Common.KEY_INTENT_MENU_ID;
@@ -28,15 +37,22 @@ public class FoodList extends AppCompatActivity {
 	String catergoryId="";
 	FirebaseRecyclerAdapter<Food, FoodViewHolder> adapter;
 
+    // Search Functionality
+    MaterialSearchBar searchBarFood;
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> searchAdapter;
+    List<String> suggestList = new ArrayList<>();
+
+
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_food_list);
 
 
 		// Init FireBase
 		firebaseDatabase = FirebaseDatabase.getInstance();
+        // Ở đây sẽ trả ra 1 Foodlist
 		databaseReference = firebaseDatabase.getReference("Foods");
 
 
@@ -51,10 +67,122 @@ public class FoodList extends AppCompatActivity {
 			loadMenuFromFireBase();
 		}
 
+		// Search
+        searchBarFood = (MaterialSearchBar) findViewById(R.id.searchBarFood);
+        searchBarFood.setHint("Enter your food");
+        loadSuggest(); // load suggest from FireBase
+
+        searchBarFood.setLastSuggestions(suggestList);
+        searchBarFood.setCardViewElevation(10);
+
+        // Chỗ này để bắt sự kiên mỗi khi nhập 1 chữ sẽ cập nhật lại list kết quả
+        searchBarFood.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                List<String> suggest = new ArrayList<String>();
+                for (String search: suggestList){
+                    if(search.toLowerCase().contains(searchBarFood.getText().toLowerCase())){
+                        suggest.add(search);
+                    }
+                }
+                searchBarFood.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        searchBarFood.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                // When Search Bar is close
+                // Restore original suggest adapter
+                if (!enabled){
+                    mRecyclerFood.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                // When Searcj finish
+                // Show result of search adapter
+                startSearch(text);
+
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
 
 	}
 
-	private void loadMenuFromFireBase() {
+    private void startSearch(CharSequence text) {
+
+        searchAdapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(
+                Food.class,
+                R.layout.item_food_recyclerlist,
+                FoodViewHolder.class,
+                databaseReference.orderByChild("Name").equalTo(text.toString())
+        ) {
+            @Override
+            protected void populateViewHolder(FoodViewHolder viewHolder, final Food model, int position) {
+                viewHolder.tvNameFood.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage()).into(viewHolder.imgFood);
+                final Food food = model;
+                viewHolder.setListener(new ItemClickListenerCustom() {
+                    @Override
+                    public void onItemClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(FoodList.this, "" + model.getName(), Toast.LENGTH_SHORT).show();
+                        // start Food detail activity
+                        Intent intentFoodDetail = new Intent(FoodList.this, FoodDetail.class);
+                        intentFoodDetail.putExtra(KEY_INTENT_FOOD_ID, searchAdapter.getRef(position).getKey());
+
+                        startActivity(intentFoodDetail);
+                    }
+                });
+            }
+        };
+        mRecyclerFood.setAdapter(searchAdapter);
+
+    }
+
+    private void loadSuggest() {
+
+        // Chỗ này nó sẽ lên database server lấy về danh sách food theo categoryID (ví dụ là danh sách tất cả món Xào)
+        // Xong sắp xếp theo MenuID
+        // Khi mà lấy được xong nó sẽ trả Call back onDataChange
+        // Ta lấy dataSnapshot của Callback và add vào danh sách gợi ý (suggestList)
+        databaseReference.orderByChild("MenuId").equalTo(catergoryId)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                            Food item = postSnapshot.getValue(Food.class);
+                            suggestList.add(item.getName());
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+    }
+
+    private void loadMenuFromFireBase() {
 		adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(
 				Food.class, R.layout.item_food_recyclerlist, FoodViewHolder.class, databaseReference.orderByChild("MenuId").equalTo(catergoryId)) {
 			@Override
